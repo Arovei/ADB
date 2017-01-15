bccaff85268b363ab4ca31e2bb4dcac0318fb497
KC <cyclonekc@gmail.com>
2017-01-15 04:36:04 -0500

    Dice Roll error checking complete

---
+++ ADB/ADB/DiceRoll.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace ADB
+{
+    public class DiceRoll
+    {
+        private static RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+        public static List<int> RollDice(List<int> input)
+        {
+            // input[0] = number of dice to roll
+            // input[1] = dice sides
+
+            List<int> output = new List<int>();
+
+            // Roll dice loop
+            for (int i = 0; i < input[0]; i++)
+            {
+                byte roll = Roller((byte)input[1]);
+                output.Add((int)roll);
+                //output[roll - 1]++;
+            }
+            return output;
+        }
+
+        public static byte Roller(byte numberSides)
+        {
+            // Create a byte array to hold the random value.
+            byte[] randomNumber = new byte[1];
+            do
+            {
+                // Fill the array with a random value.
+                rng.GetBytes(randomNumber);
+            }
+            while (!RollRange(randomNumber[0], numberSides));
+            // Return the random number mod the number
+            // of sides.  The possible values are zero-
+            // based, so we add one.
+            return (byte)((randomNumber[0] % numberSides) + 1);
+        }
+
+        private static bool RollRange(byte roll, byte numSides)
+        {
+            // There are MaxValue / numSides full sets of numbers that can come up
+            // in a single byte.  For instance, if we have a 6 sided die, there are
+            // 42 full sets of 1-6 that come up.  The 43rd set is incomplete.
+            int fullSetsOfValues = Byte.MaxValue / numSides;
+
+            // If the roll is within this range of fair values, then we let it continue.
+            // In the 6 sided die case, a roll between 0 and 251 is allowed.  (We use
+            // < rather than <= since the = portion allows through an extra 0 value).
+            // 252 through 255 would provide an extra 0, 1, 2, 3 so they are not fair
+            // to use.
+            return roll < numSides * fullSetsOfValues;
+        }
+
+        public static bool ValidDice(List<int> input, int validAmt)
+        {
+            // input[0] = number of dice to roll
+            // input[1] = dice sides
+
+            //Valid dice sides allowed
+            int[] validSides = new int[6] { 4, 6, 8, 10, 12, 20 };
+            bool validDice = false;
+
+            //Check if roll request is greater than 0 but less than allowed amount
+            if ((input[0] < validAmt+1) && (input[0] > 0))
+            {
+                validDice = true;
+                for (int i = 0; i < validSides.Length; i++)
+                {
+                    if (validSides[i] == input[1])
+                    {
+                        validDice = true;
+                        break;
+                    }
+                    else if (validSides[i] != input[1])
+                    {
+                        validDice = false;
+                    }
+                }
+            }
+            return validDice;
+        }
+    }
+}
--- ADB/ADB/Program.cs
+++ ADB/ADB/Program.cs
@@ -31,6 +31,63 @@ namespace ADB
                     await message.Channel.SendMessageAsync("pong");
             };
 
+            // Dice roll request
+            client.MessageReceived += async (message) =>
+            {
+                if (message.Content.Contains("!roll"))
+                {
+                    //Valid amount of rolls allowed
+                    int validAmount = 10;
+                    //Take the input, remove the non-numerics
+                    string input = message.Content;
+                    List<int> diceVolume = input.Split(new string[] { "D", "d" , "!roll", " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+                    //Check if dice sides and roll amounts are valid
+                    bool validSides = DiceRoll.ValidDice(diceVolume, validAmount);
+                    if (validSides == true)
+                    {
+                        //Calculate the dice roll(s)
+                        List<int> output = DiceRoll.RollDice(diceVolume);
+
+                        //Output result
+                        int totalRoll = 0;
+                        bool natural = false;
+                        string allRolls = "";
+                        string repeatRoll = input.Substring(5, input.Length - 5);
+                        List<string> rollList = new List<string>();
+                        for (int i = 0; i < output.Count; i++)
+                        {
+                            rollList.Add(output[i].ToString());
+                            if (output[i] == 20)
+                            {
+                                natural = true;
+                            }
+                            totalRoll += output[i];
+                        }
+                        for (int i = 0; i < rollList.Count; i++)
+                        {
+                            if (i + 1 == rollList.Count)
+                            {
+                                allRolls += rollList[i];
+                            }
+                            else if (i < rollList.Count)
+                            {
+                                allRolls += rollList[i] + ", ";
+                            }
+                        }
+                        await message.Channel.SendMessageAsync("`Roll results of" + repeatRoll + ": " + allRolls + "`\n```Total roll: " + totalRoll + "```");
+                        if (natural == true)
+                        {
+                            await message.Channel.SendMessageAsync("`You're a natural!`:tada:");
+                        }
+                    }
+                    else if (validSides == false)
+                    {
+                        await message.Channel.SendMessageAsync("`Error: Invalid paramters. Dice size must be 4, 6, 8, 10, 12, or 20. Roll amount must be > 0 and <= " + validAmount + ".`");
+                    }
+                }
+            };
+
             // Configure the client to use a Bot token, and use our token
             await client.LoginAsync(TokenType.Bot, token);
             // Connect the client to Discord's gateway
