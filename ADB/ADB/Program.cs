7e4f2d1a506bb8a7ac167c17b4b62a69338b95d7
KC <cyclonekc@gmail.com>
2017-01-15 22:19:41 -0500

    Don't Remember

--- ADB/ADB/Program.cs
+++ ADB/ADB/Program.cs
@@ -23,7 +23,10 @@ namespace ADB
             //Bot Token
             string token = "MjY5OTUzNjgzMjE3ODQyMTc4.C1xT0Q.JdoyNNbsPRnn-Ama--ktCbI-IHI";
 
+            //
             // Hook into the MessageReceived event on DiscordSocketClient
+            // Ping call
+            //
             client.MessageReceived += async (message) =>
             {   // Check to see if the Message Content is "!ping"
                 if (message.Content == "!ping")
@@ -31,17 +34,49 @@ namespace ADB
                     await message.Channel.SendMessageAsync("pong");
             };
 
-            // Dice roll request
+            //
+            // Help call
+            //
+            client.MessageReceived += async (message) =>
+            {
+                if (message.Content == "!help")
+                {
+                    await message.Channel.SendMessageAsync(
+                        "```Commands:```" +
+                        "  `!roll` - Usage: <Roll times>D<Dice sides>\n" +
+                        "   Rolls dice. Defaults to 1D6. Examples: !roll 2D6 | !roll 1D10"
+                        );
+                }
+            };
+
+            //
+            // Dice roll call
+            //
             client.MessageReceived += async (message) =>
             {
                 if (message.Content.Contains("!roll"))
                 {
+                    //Check if !roll was input with no parameters
+                    bool invalidCall = false;
+                    if (message.Content == "!roll") //ToLower().Contains("d")
+                    {
+                        Console.WriteLine(message.Author + " called " + message.Content + " which was an invalid call. Defaulting to 1d6.");
+                        //await message.Channel.SendMessageAsync((message.Author).Mention + " `Error: Invalid paramters. Must be in format of: <Roll amount>d<Dice sides>. Ex: 3d10`");
+                        invalidCall = true;
+                    }
                     //Valid amount of rolls allowed
                     int validAmount = 10;
                     //Take the input, remove the non-numerics
                     string input = message.Content;
                     List<int> diceVolume = input.Split(new string[] { "D", "d" , "!roll", " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
 
+                    //If call was blank, input default roll values
+                    if (invalidCall == true)
+                    {
+                        diceVolume.Add(1);
+                        diceVolume.Add(6);
+                        input = "!roll 1D6";
+                    }
                     //Check if dice sides and roll amounts are valid
                     bool validSides = DiceRoll.ValidDice(diceVolume, validAmount);
                     if (validSides == true)
@@ -51,7 +86,8 @@ namespace ADB
 
                         //Output result
                         int totalRoll = 0;
-                        bool natural = false;
+                        bool critSucc = false;
+                        bool critFail = false;
                         string allRolls = "";
                         string repeatRoll = input.Substring(5, input.Length - 5);
                         List<string> rollList = new List<string>();
@@ -60,7 +96,11 @@ namespace ADB
                             rollList.Add(output[i].ToString());
                             if (output[i] == 20)
                             {
-                                natural = true;
+                                critSucc = true;
+                            }
+                            else if (output[i] == 1)
+                            {
+                                critFail = true;
                             }
                             totalRoll += output[i];
                         }
@@ -76,17 +116,31 @@ namespace ADB
                             }
                         }
                         await message.Channel.SendMessageAsync("`Roll results of" + repeatRoll + ": " + allRolls + "`\n```Total roll: " + totalRoll + "```");
-                        if (natural == true)
+                        if (critSucc == true && critFail == false)
                         {
-                            await message.Channel.SendMessageAsync("`You're a natural!`:tada:");
+                            await message.Channel.SendMessageAsync("`Critical success!`:tada:");
                         }
+                        else if (critFail == true && critSucc == false)
+                        {
+                            await message.Channel.SendMessageAsync("`Critical failure...`:sob:");
+                        }
+                        else if (critSucc && critFail == true)
+                        {
+                            await message.Channel.SendMessageAsync("`Critical... uhh... both?`:thinking:");
+                        }
+                        Console.WriteLine(message.Author + " called " + message.Content + " resulting in a total roll of: " + totalRoll);
                     }
                     else if (validSides == false)
                     {
-                        await message.Channel.SendMessageAsync("`Error: Invalid paramters. Dice size must be 4, 6, 8, 10, 12, or 20. Roll amount must be > 0 and <= " + validAmount + ".`");
+                        Console.WriteLine(message.Author + " called " + message.Content + " which was an invalid call.");
+                        await message.Channel.SendMessageAsync((message.Author).Mention + " `Error: Invalid paramters. Dice size must be 4, 6, 8, 10, 12, or 20. Roll amount must be > 0 and <= " + validAmount + ".`");
                     }
                 }
             };
+
+            ///
+            /// Music Stream call
+            ///
 
             // Configure the client to use a Bot token, and use our token
             await client.LoginAsync(TokenType.Bot, token);
